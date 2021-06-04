0e8718176691965ea00b712d648ab7d16ebbd241
Iliadis Victor <56890377+IliadisVictor@users.noreply.github.com>
2021-06-04 14:34:45 +0300

    Update Database.java

--- WEB-INF/classes/StartitClasses/Database.java
+++ WEB-INF/classes/StartitClasses/Database.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class Database {
 
 	/* Database connection settings, change dbName, dbusername, dbpassword */
-	private final String dbServer = "195.251.249.131";
+	private final String dbServer = "localhost";
 	private final String dbServerPort = "3306";
 	private final String dbName = "ismgroup79";
 	private final String dbusername = "ismgroup79";
